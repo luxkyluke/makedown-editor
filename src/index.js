42ae6cac3c3d4d3f7b86d9a8e5a704aad32e0471
luxkyluke <rock_on77500@hotmail.fr>
2017-10-27 07:01:09 -0700

    markdown_v0.001

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import './styles/css/bootstrap.min.css';	
+import './styles/css/index.css';	
+
+import {sampleText} from './sampleText';
+
+import marked from 'marked';
+
+class App extends React.Component{
+
+	state = {
+		text: sampleText
+	}
+
+	editText = (event) => {
+		const text = event.target.value;
+		this.setState({text});
+	}
+
+	renderText = (text) =>{
+		const renderText = marked(text, {sanitize: true});
+		return { __html: renderText };
+	}
+
+	render(){
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6">
+						<textarea 
+							value={this.state.text} 
+							rows="35" 
+							className="form-control"
+							onChange={(e) => this.editText(e)}>
+						</textarea>
+					</div>
+					<div className="col-sm-6">
+						<div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
+					</div>
+				</div>		
+			</div>			
+		)
+	}
+}
+
+render(
+	<App />,
+	document.getElementById('root')
+)
